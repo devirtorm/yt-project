62dc631404114f427ae0a4762b99d5f34da1ebd1
Raúl Ponce Huerta <163025930+Yunmaster03@users.noreply.github.com>
2024-04-18 09:49:50 -0700

    videos crud semi

---
+++ backend/app/Http/Resources/VideoResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VideoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return[
+            'identificador' => $this->id,
+            'titulo' => ucfirst($this->titulo),
+            'descripcion' => ucfirst($this->descripcion),
+            'url' => $this->url,
+            'estado' => $this->estado,
+            'fk_user' => $this->fk_user,
+            'fk_categoria' => $this->fk_categoria,
+            'fecha_creado' => optional($this->created_at)->format('d-m-Y'),
+            'fecha_modificado' => optional($this->updated_at)->format('d-m-Y')
+        ];
+    }
+}
